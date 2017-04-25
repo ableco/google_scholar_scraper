45dee7eb7a5f7107b7033f08a67f78591f3c583f
Ricardo Díaz <ricardotk002@gmail.com>
2017-04-25 16:02:59 -0500

    GoogleScholarScraper::Request now uses RestClient::Request
    RestClient::Request gives us more control when we need to make requests.
    We can set the ssl verifiation and add the proxy URL per request, so we
    don't need to set it globally in RestClient.proxy which may lead to
    unexpected results for other gems that rely on it.

--- lib/google_scholar_scraper/request.rb
+++ lib/google_scholar_scraper/request.rb
@@ -9,29 +9,42 @@ module GoogleScholarScraper
 
     def initialize(path, last_request = nil)
       @path = path
-      @previous_user_agent = last_request.user_agent
+      @previous_user_agent = last_request.user_agent if last_request
     end
 
     def get
-      Nokogiri::HTML(RestClient.get(request_url, user_agent: user_agent))
+      logger.info("GoogleScholarScraper: Requesting #{path} with user agent #{user_agent}")
+      Nokogiri::HTML(
+        RestClient::Request.execute(
+          url: request_url,
+          method: :get,
+          verify_ssl: false,
+          user_agent: user_agent,
+          proxy: GoogleScholarScraper.configuration.proxy_url
+        )
+      )
     end
 
     def user_agent
-      new_user_agent = nil
-      loop do
-        new_user_agent = UserAgents.rand
-        return if new_user_agent != previous_user_agent
-      end
-      new_user_agent
+      @user_agent ||= generate_user_agent
     end
 
     private
 
-    attr_reader :path
-    attr_writer :previous_user_agent
+    attr_reader :path, :previous_user_agent
+
+    def generate_user_agent
+      tmp_user_agent = UserAgents.rand
+      tmp_user_agent = generate_user_agent if tmp_user_agent == previous_user_agent
+      tmp_user_agent
+    end
 
     def request_url
       "#{BASE_URL}#{path}"
+    end
+
+    def logger
+      GoogleScholarScraper.configuration.logger
     end
   end
 end
